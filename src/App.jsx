fadb364cd38d6998f08de7711003a69d6e11e294
LaimaLP <pileliene.laima@gmail.com>
2023-12-09 17:21:37 +0200

    log component

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,61 @@ import { Player } from "./components/Player";
 import "./App.css";
 import { GameBoard } from "./components/GameBoard";
 import { useState } from "react";
-import style from "./components/Header.module.css";
+import { Log } from "./components/Log";
 
+
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = "X";
+
+  if(gameTurns.length > 0 && gameTurns[0].player === "X"){
+    currentPlayer = "O"
+  }
+}
 function App() {
-  const [activePlayer, setActivePlayer]= useState("X");
-  
-  function handleSelectSq(){
-    setActivePlayer((curActivePlayer)=> curActivePlayer === "X"? "O": "X" );
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [activePlayer, setActivePlayer] = useState("X");
+const activePlayer = deriveActivePlayer(gameTurns);
+
+  function handleSelectSq(rowIdx, colIdx) {
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+
+    setGameTurns((prevTurns) => {
+ const currentPlayer =deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIdx, col: colIdx }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
     <main>
       <div className="style.gameContainer">
         <Header />
-        <div className='playerInfoGameBoard'>
+        <div className="playerInfoGameBoard">
           <ol id="players" className="higlightPayer">
-            <Player defaultName="Player 1" symbol="X" isActive={activePlayer === "X"} />
-            <Player defaultName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
+            <Player
+              defaultName="Player 1"
+              symbol="X"
+              isActive={activePlayer === "X"}
+            />
+            <Player
+              defaultName="Player 2"
+              symbol="O"
+              isActive={activePlayer === "O"}
+            />
           </ol>
-          <GameBoard onSelectSq={handleSelectSq} activePlayerSymbol={activePlayer} />
+          <GameBoard
+            onSelectSq={handleSelectSq}
+            turns={gameTurns}
+          />
         </div>
       </div>
-      LOG
+      <Log turns ={gameTurns}/>
     </main>
   );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,5 +1,4 @@
 import style from "./Header.module.css";
-import { useState } from "react";
 
 const initialGameBoard = [
   [null, null, null],
@@ -7,19 +6,29 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export function GameBoard({ onSelectSq, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export function GameBoard({ onSelectSq, turns }) {
 
-  function handleSelectSq(rowIdx, colIdx) {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIdx][colIdx] = activePlayerSymbol;
-      return updatedBoard;
-    });
-    onSelectSq();
+  let gameBoard = initialGameBoard;
+
+  for(const turn of turns){
+    const {square, player} = turn;
+    const {row, col} = square;
+
+    gameBoard[row][col] = player;
   }
+
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // function handleSelectSq(rowIdx, colIdx) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIdx][colIdx] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
+  //   onSelectSq();
+  // }
 
   return (
     <>
@@ -29,10 +38,8 @@ export function GameBoard({ onSelectSq, activePlayerSymbol }) {
             <ol>
               {row.map((playerSymbol, colIdx) => (
                 <li key={colIdx}>
-                  <button
-                    className={style.playerSymbolBut}
-                    onClick={() => handleSelectSq(rowIdx, colIdx)}
-                  >
+                  <button onClick={()=> onSelectSq(rowIdx, colIdx)}
+                    className={style.playerSymbolBut} disabled={playerSymbol !== null}>
                     {playerSymbol}
                   </button>
                 </li>
