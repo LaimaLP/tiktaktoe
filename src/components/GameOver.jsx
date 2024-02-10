e58bf58e9a969c2e3783602dc5bd5e24cb1fdd9c
LaimaLP <pileliene.laima@gmail.com>
2024-02-10 16:53:05 +0200

    removed game over

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,7 +1,6 @@
 export default function GameOver({ winner, onRestart }) {
   return (
     <div className="gameOver">
-      <h2>Game Over!</h2>
       {winner && <p>{winner} won!</p>}
       {!winner && <p>It's a draw !</p>}
 
